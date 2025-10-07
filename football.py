0d8ab3e6a1092e633459b9a21d3bd57000fd3bc6
Alankar Tripathi <168866152+Alankaar63@users.noreply.github.com>
2025-10-07 15:16:24 +0530

    completely updated this project from our side today

--- football.py
+++ football.py
@@ -3,38 +3,31 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Sample Data
-teams = [
-    {"name": "Real Madrid", "country": "Spain", "titles": 14},
-    {"name": "Manchester United", "country": "England", "titles": 3},
-    {"name": "Bayern Munich", "country": "Germany", "titles": 6},
-    {"name": "AC Milan", "country": "Italy", "titles": 7},
-    {"name": "SIT Team", "country": "Susgaon", "titles": 4},
-    {"name": "AIU Team", "country": "Pimpli Saudagar", "titles": 10},
-    {"name": "SIU Team", "country": "Baner", "titles": 6}
-]
-
 players = [
     {"name": "Lionel Messi", "club": "Inter Miami", "country": "Argentina"},
     {"name": "Cristiano Ronaldo", "club": "Al Nassr", "country": "Portugal"},
     {"name": "Kylian Mbappé", "club": "PSG", "country": "France"},
-    {"name": "Erling Haaland", "club": "Manchester City", "country": "Norway"},
-    {"name": "ASHU", "club": "SITFC", "country": "India"},
-    {"name": "Labdhi", "club": "AIUFC", "country": "India"},
-    {"name": "Alan", "club": "FCSIU", "country": "India"}
-
+    {"name": "Erling Haaland", "club": "Manchester City", "country": "Norway"}
 ]
 
-@app.route("/")
-def home():
+teams = [
+    {"name": "Real Madrid", "country": "Spain", "titles": 14},
+    {"name": "Manchester United", "country": "England", "titles": 3},
+    {"name": "Bayern Munich", "country": "Germany", "titles": 6},
+    {"name": "AC Milan", "country": "Italy", "titles": 7}
+]
+
+@app.route('/')
+def index():
     return render_template("index.html")
 
-@app.route("/teams")
-def show_teams():
-    return render_template("teams.html", teams=teams)
-
-@app.route("/players")
+@app.route('/players')
 def show_players():
     return render_template("players.html", players=players)
+
+@app.route('/teams')
+def show_teams():
+    return render_template("teams.html", teams=teams)
 
 if __name__ == "__main__":
     app.run(debug=True)
